5206eab2fa7797fa72866955195d075ea707d345
RenNanase <dhzrnaaa@gmail.com>
2025-01-08 14:59:52 +0800

    fix: update vite config to use correct entry file path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+                css: 'resources/css/app.css',
+            },
+        },
+    },
 });
