4e14579bc1fac30f901037bdc0f675a30f1fb800
RenNanase <dhzrnaaa@gmail.com>
2025-01-08 15:05:20 +0800

    fix: update file extension for JSX support

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
@@ -15,7 +15,7 @@ export default defineConfig({
         manifest: true,
         rollupOptions: {
             input: {
-                app: 'resources/js/app.js',
+                app: 'resources/js/app.jsx',
                 css: 'resources/css/app.css',
             },
         },
