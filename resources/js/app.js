862074417d12ea90066aabaf37ebb1e5b41b753f
RenNanase <dhzrnaaa@gmail.com>
2025-01-08 15:15:36 +0800

    refactor: remove React dependencies and simplify to vanilla JS

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import '../css/app.css';
+import './bootstrap';
+
+// Add your JavaScript code here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('App is running');
+    // Your JavaScript code goes here
+});
